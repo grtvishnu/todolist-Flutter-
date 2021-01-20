d99cd019ec20e3f0612ae9df527729d6e00cfe3d
grtvishnu <optra7@gmail.com>
2021-01-20 12:43:15 +0530

    Add

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(theme: ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Colors.blue,
+  accentColor: Colors.orange
+),
+  home: Myapp(),
+));
+class Myapp extends StatefulWidget {
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  List todos = List();
+  String input = "";
+  @override
+  void initState(){
+    super.initState();
+    todos.add("Item1");
+    todos.add("Item2");
+    todos.add("Item3");
+    todos.add("Item4");
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Todos"),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed:
+      (){
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8)),
+            title: Text("Add TodoList"),
+            content: TextField(
+              onChanged: (String value){
+                input = value;
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: (){
+                    setState(() {
+                      todos.add(input);
+                    });
+                    Navigator.of(context).pop();
+              }, child: Text("Add"))
+            ],
+          );
+        });
+      }, child: Icon(Icons.add,
+        color: Colors.white,),),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context,
+              int index) {
+        return Dismissible(
+            key: Key(todos[index]),
+            child: Card(
+              elevation: 4,
+              margin: EdgeInsets.all(8),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          child: ListTile(
+            title: Text(todos[index]),
+            trailing: IconButton(icon: Icon(Icons.delete, color: Colors.red,
+            ),
+              onPressed: (){
+              setState(() {
+                todos.removeAt(index);
+              });
+              }),
+          ),
+        ));
+      }),
+    );
+  }
+}
